84f54221e656176789bf95b0f1783592429ba20a
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-07-25 10:34:56 +0000

    Add /api/v1/stats endpoint to return object counts

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,13 +7,13 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/api/v1/stats', methods=['GET'])
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
     """Retrieves the number of each objects by type."""
     stats = {
